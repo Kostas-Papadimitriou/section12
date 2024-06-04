0831392f3818d53e491d3ca94497dd4b07c63518
Konstantinos Papadimitriou <papadimitriou@synelixis.com>
2024-06-04 17:21:45 +0300

    image picker and start of locatiojn

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,14 +1,67 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, Button, Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { launchCameraAsync, useCameraPermissions } from "expo-image-picker";
+import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
 
 const ImagePicker = () => {
+  const [pickedImage, setPickedImage] = useState("");
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficent Permissions",
+        "You need to grant camera permissions"
+      );
+      return false;
+    }
+    return true;
+  }
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    setPickedImage(image);
+  }
+
+  let imagePreview = <Text>No image taken yet</Text>;
+  if (pickedImage) {
+    imagePreview = <Image source={{ uri: pickedImage.uri }} />;
+  }
+
   return (
     <View>
-      <Text>ImagePicker</Text>
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton onPress={takeImageHandler} icon="camera">
+        Take Image
+      </OutlinedButton>
     </View>
   );
 };
 
 export default ImagePicker;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: "200px",
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+});
